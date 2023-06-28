a2841f84d8d26257daa8c0e6729a5a9003728cfb
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-06-28 21:16:48 +0200

    Add additional arguments.

--- subnet.main.tf
+++ subnet.main.tf
@@ -1,7 +1,25 @@
 resource "azurerm_subnet" "this" {
-  name                 = coalesce(var.name, replace(join(var.separator, local.names), " ", var.space))
-  virtual_network_name = coalesce(var.virtual_network_name, replace(join(var.separator, local.names), " ", var.space))
-  resource_group_name  = coalesce(var.resource_group_name, replace(join(var.separator, local.names), " ", var.space))
-  address_prefixes     = var.address_prefixes
-  service_endpoints    = var.service_endpoints
+  address_prefixes = var.address_prefixes
+
+  dynamic "delegation" {
+    for_each = {
+      for delegation in var.delegations :
+      delegation.name => delegation
+      if delegation.name != null
+    }
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name    = delegation.value.service_delegation.name
+        actions = delegation.value.service_delegation.actions
+      }
+    }
+  }
+
+  name                                          = coalesce(var.name, replace(join(var.separator, local.names), " ", var.space))
+  private_endpoint_network_policies_enabled     = var.private_endpoint_network_policies_enabled
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
+  resource_group_name                           = coalesce(var.resource_group_name, replace(join(var.separator, local.names), " ", var.space))
+  service_endpoints                             = var.service_endpoints
+  virtual_network_name                          = coalesce(var.virtual_network_name, replace(join(var.separator, local.names), " ", var.space))
 }
--- subnet.vars.tf
+++ subnet.vars.tf
@@ -1,23 +1,60 @@
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group in which to create the subnet."
-  default     = ""
-}
-
-variable "virtual_network_name" {
-  type        = string
-  description = "The name of the virtual network to which to attach the subnet."
-  default     = ""
-}
-
 variable "address_prefixes" {
   type        = list(string)
   description = "The list of address prefixes to use for the subnet."
-  default     = []
+  default     = null
+}
+
+variable "delegations" {
+  type = list(object({
+    name = string # A name for this delegation.
+    service_delegation = object({
+      name    = string                 # The name of service to delegate to.
+      actions = optional(list(string)) # A list of Actions which should be delegated.
+    })
+  }))
+  description = "A delegation block."
+  default = [
+    {
+      name = null
+      service_delegation = {
+        name = null
+      }
+    }
+  ]
+}
+
+variable "private_endpoint_network_policies_enabled" {
+  type        = bool
+  description = "Enable or Disable network policies for the private endpoint on the subnet."
+  default     = null
+}
+
+variable "private_link_service_network_policies_enabled" {
+  type        = bool
+  description = "Enable or Disable network policies for the private link service on the subnet."
+  default     = null
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to create the subnet."
+  default     = null
 }
 
 variable "service_endpoints" {
   type        = list(string)
   description = "The list of Service endpoints to associate with the subnet."
-  default     = []
+  default     = null
+}
+
+variable "service_endpoint_policy_ids" {
+  type        = list(string)
+  description = "The list of IDs of Service Endpoint Policies to associate with the subnet."
+  default     = null
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "The name of the virtual network to which to attach the subnet."
+  default     = null
 }
