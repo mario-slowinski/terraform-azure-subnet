7580b3e52b9d4b25ffc440d19e376bd2499cd276
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-18 22:21:57 +0200

    Use "local.name" in "azurerm_subnet" resource.

--- subnet.main.tf
+++ subnet.main.tf
@@ -16,11 +16,11 @@ resource "azurerm_subnet" "this" {
     }
   }
 
-  name                                          = coalesce(var.name, replace(join(var.separator, local.names), " ", var.space))
+  name                                          = coalesce(var.name, local.name)
   private_endpoint_network_policies_enabled     = var.private_endpoint_network_policies_enabled
   private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
-  resource_group_name                           = coalesce(var.resource_group_name, replace(join(var.separator, local.names), " ", var.space))
+  resource_group_name                           = coalesce(var.resource_group_name, local.name)
   service_endpoints                             = var.service_endpoints
   service_endpoint_policy_ids                   = var.service_endpoint_policy_ids
-  virtual_network_name                          = coalesce(var.virtual_network_name, replace(join(var.separator, local.names), " ", var.space))
+  virtual_network_name                          = coalesce(var.virtual_network_name, local.name)
 }
