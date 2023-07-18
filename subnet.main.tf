7a2b4c249e2e375c476abebe09f64ff13e7870e5
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-18 22:20:21 +0200

    Add "service_endpoint_policy_ids" to "azurerm_subnet" resource.

--- subnet.main.tf
+++ subnet.main.tf
@@ -21,5 +21,6 @@ resource "azurerm_subnet" "this" {
   private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
   resource_group_name                           = coalesce(var.resource_group_name, replace(join(var.separator, local.names), " ", var.space))
   service_endpoints                             = var.service_endpoints
+  service_endpoint_policy_ids                   = var.service_endpoint_policy_ids
   virtual_network_name                          = coalesce(var.virtual_network_name, replace(join(var.separator, local.names), " ", var.space))
 }
